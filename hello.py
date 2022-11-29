c5d0a50d05f397cfab78d326ff6085bf5dd9b89d
gnjeri <gnjeri403@gmail.com>
2022-11-29 21:20:19 +0300

    add project files

---
+++ hello.py
@@ -0,0 +1,37 @@
+print("Hello 5  Babe")
+print("Dad is called Julius")
+print("My sister is about to fly to Dubai")
+
+
+# Arithmetic operators
+# Addition of 50 + 5.4
+print(50 + 5.4)
+
+# Multiplication
+print(3 * 5)
+
+# Division
+print(10 / 2)
+
+#Subtraction
+print(10 - 2 )
+
+#Modulus(remainder)
+print(11 % 2)
+
+# Variables
+accountAmount = 500 #integer int
+
+withdrawAmount = 150 # int
+
+# withdraw  action
+
+balance = accountAmount - withdrawAmount
+
+accountOwner = "Grace Njeri" #string
+
+taxRate = 16.5 # float
+
+print( type (balance) )
+print(type(accountOwner))
+print (type(taxRate))
---
+++ syntax.py
@@ -0,0 +1,11 @@
+number = 5
+
+if  (number > 2):
+  print("Number greater than 2 ")
+elif (number > 10): 
+  print("Number greater than 10")
+elif (number < 1):
+  print("Number less than 1")
+else:
+  print("Unknown Number")
+  
